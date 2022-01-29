2e70002b6e2eb10bfd6c4e51f84cc82846d5ea7a
poisonborz <mail@poisonborz.com>
2022-01-29 15:14:57 +0100

    fix tests for possible concurrency

--- tests/_tools.js
+++ tests/_tools.js
@@ -15,7 +15,7 @@ export const fileCleanup = (testOutputDirName, fileNamePartial) => {
             if (err) throw err
 
             for (const file of files) {
-                if (file.includes(fileNamePartial)) {
+                if (file.includes(fileNamePartial) && fs.existsSync(file)) {
                     fs.unlink(file, err => {
                         if (err) throw err
                     })
